37f94a8ec9c644015149d07de744aba853c42205
Gordon Oh <gordonoh@yahoo.com.sg>
2026-05-26 01:45:13 +0800

    fix script bug

--- luma_reminder.sh
+++ luma_reminder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /root/tg_sentinel || exit 1
+
 exec > >(tee -a /root/tg_sentinel/luma_reminder_output.log) 2>&1
 
 source venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
